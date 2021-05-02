e00a9d1b67f728895609a2a30a435ccb02632e08
Ladislav Sopko <ladislav.sopko@gmail.com>
2021-05-02 16:10:48 +0200

    wip

--- JohnKnoop.MongoRepository.IntegrationTests/LaunchSettingsFixture.cs
+++ JohnKnoop.MongoRepository.IntegrationTests/LaunchSettingsFixture.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using MongoDB.Driver;
@@ -17,15 +18,22 @@ namespace JohnKnoop.MongoRepository.IntegrationTests
 				var jObject = JObject.Load(reader);
 
 				var variables = jObject
-					.GetValue("profiles")
+					.GetValue("profiles");
+				List<JProperty> src;
+
+				if (variables != default) {
 					//select a proper profile here
-					.SelectMany(profiles => profiles.Children())
+					src = variables.SelectMany(profiles => profiles.Children())
 					.SelectMany(profile => profile.Children<JProperty>())
 					.Where(prop => prop.Name == "environmentVariables")
 					.SelectMany(prop => prop.Value.Children<JProperty>())
 					.ToList();
+				} else
+                {
+					src = new List<JProperty>();
+                }
 
-				foreach (var variable in variables)
+				foreach (var variable in src)
 				{
 					Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
 				}
